212327d26e50a80d14934ab426f212385f780076
Jason Divock <jdivock@gmail.com>
2013-06-05 21:57:20 -0400

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,11 @@
+/*global Ember */
+
+var Todos = window.Todos = Ember.Application.create();
+
+/* Order and include as you please. */
+ require('app/scripts/routes/*');
+ require('app/scripts/controllers/*');
+ require('app/scripts/models/*');
+// require('app/scripts/views/*');
+
+
