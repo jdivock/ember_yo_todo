506433a9fd59d83597f75044d4c1033a3834d6e9
Jason Divock <jdivock@gmail.com>
2013-06-09 11:46:12 -0400

    fixed some stuff, todo count, new todo

--- app/scripts/controllers/todos_controller.js
+++ app/scripts/controllers/todos_controller.js
@@ -1,4 +1,29 @@
+Todos.TodosController = Ember.ArrayController.extend({
+  createTodo: function(){
+    var title = this.get('newTitle');
+    if(!title.trim()) {return;}
+
+    var todo = Todos.Todo.createRecord({
+      title: title,
+      isCompleted: false
+    });
+    this.set('newTitle','');
+
+    todo.save();
+  },
+
+    remaining: function () {
+    return this.filterProperty('isCompleted', false).get('length');
+  }.property('@each.isCompleted'),
+
+  inflection: function () {
+    var remaining = this.get('remaining');
+    return remaining === 1 ? 'item' : 'items';
+  }.property('remaining')
+});
+
 Todos.TodoController = Ember.ObjectController.extend({
+
   isCompleted: function(key, value){
     var model = this.get('model');
 
